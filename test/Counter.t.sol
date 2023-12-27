90eca4f192bdb9584c3532b6350b7ed6a2180c4b
saadraja-aleh <saadraja@aleh.tech>
2023-12-27 19:14:07 +0500

    initial commit

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.17;
 
-import {Test, console2} from "forge-std/Test.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@uniswap/periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+import "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterTest is Test {
@@ -14,11 +18,13 @@ contract CounterTest is Test {
 
     function test_Increment() public {
         counter.increment();
+        console.log(counter.number());
         assertEq(counter.number(), 1);
     }
 
     function testFuzz_SetNumber(uint256 x) public {
         counter.setNumber(x);
+        console.log(counter.number());
         assertEq(counter.number(), x);
     }
 }
